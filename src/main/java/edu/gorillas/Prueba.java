ff44310f65435c9d83232bf6d5577be1c65f1f3e
nasua <acollazocacho@gmail.com>
2024-05-03 12:35:22 +0200

    build: crear caso test suma

--- src/main/java/edu/gorillas/Prueba.java
+++ src/main/java/edu/gorillas/Prueba.java
@@ -1,7 +1,12 @@
 package edu.gorillas;
 
-public class Main {
+public class Prueba {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+    }
+
+    public int suma(int operando1, int operando2) {
+        return operando1 + operando2;
     }
 }
--- src/test/java/PruebaTest.java
+++ src/test/java/PruebaTest.java
@@ -1,8 +1,21 @@
+import edu.gorillas.Prueba;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class MainTest {
-    @Test
-    void name() {
+public class PruebaTest {
+    @BeforeEach //Alt+Ins SetUp method -> crea el Before, aqui se mete la creacion del objeto que vamos a testear
+    void setUp() {
+        Prueba prueba1 = new Prueba();
+    }
+
+
+    @Test //Alt+Ins - Test Metod -> crea test
+    void suma() {
+        Prueba prueba1 = new Prueba();
+        int resultado = prueba1.suma(2,3);
+     assertEquals(5, resultado);
 
     }
 
